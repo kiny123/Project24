ea3c388b7e345d95f8f422493788a8ae69a79a3d
kiny123 <89192734912nik@gmail.com>
2023-03-15 23:29:02 +0300

    1

--- Project24.playground/Contents.swift
+++ Project24.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var str = "Hello, playground"
+let weather = "it's going to rain"
+print(weather.capitalized)
+
+extension String {
+    var capitalizedFirst: String {
+        guard let firstLetter = self.first else { return "" }
+        return firstLetter.uppercased() + self.dropFirst()
+    }
+}
+
+let input = "Swift is like Objective-C without the C"
+input.contains("Swift")
+
+let languages = ["Python", "Ruby", "Swift"]
+languages.contains("Swift")
+
+
+extension String {
+    func containsAny(of array: [String]) -> Bool {
+        for item in array {
+            if self.contains(item) {
+                return true
+            }
+        }
+
+        return false
+    }
+}
+
+input.containsAny(of: languages)
+
+languages.contains(where: input.contains)
+
